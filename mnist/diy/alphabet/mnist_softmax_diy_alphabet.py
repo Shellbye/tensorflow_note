e064c094e0aeafb175118b575357eea210cdbfbb
shellbye <shellbye.com@gmail.com>
2017-08-22 10:15:51 +0800

    alphabet ok as 0.8

--- mnist/diy/alphabet/mnist_softmax_diy_alphabet.py
+++ mnist/diy/alphabet/mnist_softmax_diy_alphabet.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import codecs
+import string
 
 import tensorflow as tf
 
@@ -30,7 +30,7 @@ def train(model_save_path):
     sess = tf.InteractiveSession()
     tf.global_variables_initializer().run()
 
-    captcha = get_captcha("/Users/shellbye/Projects/tensorflow_note/utils/")
+    captcha = get_captcha("/Users/shellbye/Projects/tensorflow_note/data/mnist/diy/alphabet/")
     for _ in range(1000):
         batch_xs, batch_ys = captcha.train_next_batch(100, alpha=True)
         sess.run(train_step, feed_dict={x: batch_xs, y_real: batch_ys})
@@ -45,8 +45,30 @@ def train(model_save_path):
     print(sess.run(accuracy, feed_dict=fd))
 
 def real_test(model_save_path, testing_data):
-    pass
+    x = tf.placeholder(tf.float32, [None, 784])
+    W = tf.Variable(tf.zeros([784, 26]))
+    b = tf.Variable(tf.zeros([26]))
+    y = tf.matmul(x, W) + b
+
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        saver = tf.train.Saver()
+        saver.restore(sess, model_save_path)
+        right, wrong = 0, 0
+        with open(testing_data, "r") as f:
+            for line in f:
+                line = line.replace("\n", "")
+                image, label = line.split(" ")
+                image_data = load_image(image)
+                pre = sess.run(tf.arg_max(y, 1), feed_dict={x: [image_data]})[0]
+                if pre == string.ascii_uppercase.index(label):
+                    right += 1
+                else:
+                    wrong += 1
+                    print("predict {} {}, real {}".format(image, pre, label))
+    print("right:{}, wrong:{}".format(right, wrong))
 
 if __name__ == '__main__':
-    train("models_alphabet/model.ckpt")
-    # real_test("models_alphabet/model.ckpt", "../utils/testing_set.txt")
+    # train("models_alphabet/model.ckpt")
+    real_test("models_alphabet/model.ckpt",
+              "/Users/shellbye/Projects/tensorflow_note/data/mnist/diy/alphabet/testing_set.txt")
